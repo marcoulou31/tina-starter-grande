7441ee8eb7f56e885ec1548003f612bdb11c87bf
Marc Tanguay <dev@xgestion.com>
2020-02-29 02:52:01 -0500

    give CMS Option in production

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,7 @@ module.exports = {
       resolve: "gatsby-plugin-tinacms",
       options: {
         sidebar: {
-          hidden: process.env.NODE_ENV === "production",
+          hidden: process.env.NODE_ENV === "productionOff",
           position: "displace",
           theme: {
             color: {
