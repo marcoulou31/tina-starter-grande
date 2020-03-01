bc592a610404466530710ae2b65638511aaaccd1
Marc Tanguay <dev@xgestion.com>
2020-03-01 05:44:30 -0500

    add git config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,9 @@
 const theme = require("./content/settings/theme.json")
 const site = require("./content/settings/site.json")
 
+const path = require("path")
+const REPO_ABSOLUTE_PATH = path.join(process.cwd(), "../..")
+
 module.exports = {
   plugins: [
     `gatsby-transformer-sharp`,
@@ -26,7 +29,12 @@ module.exports = {
         plugins: ["gatsby-tinacms-remark", {
           resolve: "gatsby-tinacms-git",
           options: {
-            sshKey: process.env.SSH_KEY
+            pathToRepo: REPO_ABSOLUTE_PATH,
+            defaultCommitMessage: 'Edited with TinaCMS',
+            defaultCommitName: 'tina-starter-grande',
+            defaultCommitEmail: 'dev@xgestion.com',
+            pushOnCommit: true,
+            sshKey: "c3NoLXJzYSBBQUFBQjNOemFDMXljMkVBQUFBREFRQUJBQUFDQVFEUVo4WWdvV0gzY0dnSlBaSk0yaHkrdTFRN3VSYmNvNlFicDZYRElsVU13WkZyQTNSNEJTajIzV1ZsU3RhYnhUdGxLTGZmellVWVMvbE16ekhSWTRXYnNaTEh2L0lRNXkwZnRyenpwNmtySlFYTmZKWmNDM0RxYzJTcVgwMTBwb3g3WjdiaERiS24wS29kRi9DY2Y4TlpyN29CUlQ5dEp5QmE1V2t5cW5WUi9BSS8ydjRaU0ZIeklxT3E4Vk40TGo1dW02NFJPYkxkQmRTQS9lWEhCYjNPKzg3Z3JZY0NUYkxKV0d6VGM5WFVwV05yT3dIdDJFYkRCenNTYWpIU1NWdWNOOGd0aDMza0JoOVlGeWtucDh4UGpIcWszMUlFeE5meVRSa0ZyVWNCQmlUZ1lROFkwdTNuMWRiWjloMkMzSzRUZGwwRklyY0N5S0RjcTBYb2pCN3pCd3hmUWF5UG4rRXBYNWVud0YvVkttQS9YOGdYL0tCZHRjQjZWZ2JETktzRnhLRlRyQTdra0wxeWZNeGdGNlhxbEpHQkk2d1hVOXhySFhHcEFIS3BSbXIwUi9EUGNzZWpUenArSlAxaXpRemhRdUd3ZUVraU1QOFc4Nk96WUxRSjltUXJNb1NTNy9mT3BIU2FLWDRESE92YlUrSzZhalh1em44Ui85S3FpSndaci9NVnN3MDFRU2kyMkJvazIrMGhPNDQyRHB6dlNDclAyeXpTNkNrYjBjSVI1aWtveDlLaFR4YjFFZytldGZwcHJiRWNsdXpaTVF5TFlLS2QrTGdpVWt1b2MwOUVJTXIzQS9nOWM0czBSam8zc3N4dUZKcmRZYTkrdnZlRWY5Z0E4d3hGZ0ZGaFUrejVkbVJWa0ZZT2pFR0NhZklzd1g2NzFnZ3FiM2c5ZlE9PSBkZXZAeGdlc3Rpb24uY29t"
           },
         },],
       },
